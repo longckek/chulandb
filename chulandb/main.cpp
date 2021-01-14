0bd09e45d624537220516fbb30e57e9719016a57
longckek <longckek@gmail.com>
2021-01-14 21:44:39 +0700

    alo1234

--- chulandb/main.cpp
+++ chulandb/main.cpp
@@ -1,9 +1,4 @@
-//
-//  main.cpp
-//  chulandb
-//
-//  Created by Long Nguyễn Kim on 14/01/2021.
-//
+
 
 #include <iostream>
 
@@ -11,4 +6,5 @@ int main(int argc, const char * argv[]) {
           // insert code here...
           std::cout << "Hello, World!\n";
           return 0;
+          //fuck dmemmmm
 }
